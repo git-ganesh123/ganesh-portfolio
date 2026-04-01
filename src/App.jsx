35582fc47e9c32450168e0038ec0d39a0907738e
Ganesh Bhat <ganeshmb05@gmail.com>
2026-04-01 15:53:13 +1100

    profile photo update

--- src/App.jsx
+++ src/App.jsx
@@ -540,7 +540,7 @@ function HomePage({ setCurrentPage }) {
             border: "1px solid rgba(100,181,246,0.08)",
           }}>
             <motion.img
-              src="file:///Users/ganesh/Desktop/aboutme.png"
+              src="/aboutme.png"
               alt="Ganesh portrait"
               style={{ width: "100%", aspectRatio: "3/4", objectFit: "cover", display: "block" }}
               whileHover={{ scale: 1.03 }}
